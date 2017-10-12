fdd66b23af2b0d5e6ba286baa57bed9543abf6c6
Xiaolong Wang <wangxl@bjtckj.cn>
2017-10-12 16:23:31 +0800

    完善菜单事件动作，增加选中样式

--- src/app/layout/navigation/navigation.component.ts
+++ src/app/layout/navigation/navigation.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-navigation',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationComponent implements OnInit {
 
-  constructor() { }
+  open = [false, false, false, false];
+
+  constructor() {
+  }
 
   ngOnInit() {
+  }
+
+  _toggleMenu(index) {
+    this.open[index] = !this.open[index];
   }
 
 }
